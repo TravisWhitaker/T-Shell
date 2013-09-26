3101c55789a51146709ef7190043691a80ec94e6
Tyler Cromwell <tyler@csh.rit.edu>
2013-09-26 02:02:40 -0400

    Added gcc's readline compiler flag

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # This file is used to build my shell
 clear
 echo "Building..."
-gcc -g --std=gnu99 -ggdb tsh.c ../lib/* -o tsh
+gcc -g --std=gnu99 -ggdb -lreadline tsh.c ../lib/* -o tsh
 echo "Build Completed."
