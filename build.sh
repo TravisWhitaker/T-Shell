416153531f9ee982e972211dca6be5b284d7798e
Tyler Cromwell <tyler@csh.rit.edu>
2013-10-08 15:10:45 -0400

    Made some minor changes

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 # This file is used to build my shell
 clear
 echo "Building..."
-gcc -g --std=gnu99 -ggdb -lreadline tsh.c ../lib/* -o tsh
+gcc -g --std=gnu99 -ggdb -lreadline tsh.c ../lib/* -o ./tsh
+#sudo cp ./tsh /bin/
 echo "Build Completed."
