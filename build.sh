36d5d06c7f9920c58947b0fb260b67f4cc9c250f
Tyler Cromwell <tyler@csh.rit.edu>
2013-12-18 01:41:41 -0500

    Forgot to remove build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This file is used to build my shell
-clear
-echo "Building..."
-gcc -g -std=gnu99 -Wall -ggdb -I ~/Code/C/Projects/include -lreadline tsh.c ../lib/* -o ./tsh
-#sudo cp ./tsh /bin/
-echo "Build Completed."
