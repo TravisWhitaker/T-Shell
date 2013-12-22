a162ea8aeb70c76311126341ee82648fdf0b8594
Tyler Cromwell <tyler@csh.rit.edu>
2013-12-21 22:14:36 -0500

    Removed memory test script from version control

--- memtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This file is used to run my shell with Valgrind
-clear
-echo "Running..."
-valgrind --tool=memcheck --leak-check=full --show-reachable=yes ./tsh
-echo "Test complete!"
