43e726eddb5f49971e15fdb9d9b405b378fc4a96
Tyler Cromwell <tyler@csh.rit.edu>
2013-12-31 20:50:34 -0500

    Forgot to commit changes to this file

--- include/redirection.h
+++ include/redirection.h
@@ -4,8 +4,8 @@
 #define REDIRECT_SUCCESS 1
 #define REDIRECT_FAILURE 0
 
-int redirect_in(int argc, char* argv[]);
-int redirect_out(int argc, char* argv[]);
-int redirect_pipe(int argc, char* argv[]);
+extern int redirect_in(int argc, char* argv[]);
+extern int redirect_out(int argc, char* argv[]);
+extern int redirect_pipe(int argc, char* argv[]);
 
 #endif
