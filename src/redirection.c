1fe15484ff9f39c48bb5705dd7888cf7bf4e06b8
Tyler Cromwell <tyler@csh.rit.edu>
2014-01-04 14:09:41 -0500

    Documented redirection.c

--- src/redirection.c
+++ src/redirection.c
@@ -11,14 +11,23 @@
 #include "redirection.h"
 
 typedef struct redirection_symbol {
-	char* symbol;
-	unsigned int index;
+	char* symbol;		// The symbol itself.
+	unsigned int index; // Its index in argv.
 } redir_sym;
 
+/*
+ * Returns the arguments within a range (start <= N <= end).
+ * Argument(s):
+ *   char* argv[]: The command line arguments.
+ *   int start: Index of the first argument.
+ *   int end: Index of the end argument
+ * Returns:
+ *   An array of command line arguments.
+ */
 static char** args_in_range(char* argv[], int start, int end) {
 	char** args = NULL;
 	unsigned int index;
-	for (unsigned int i = start; i < (end+1); i++) {
+	for (unsigned int i = start; i <= end; i++) {
 		index = i-start;
 		args = realloc(args, (index+1) * sizeof(char*));
 		if (i == end) args[index] = NULL;
@@ -27,6 +36,14 @@ static char** args_in_range(char* argv[], int start, int end) {
 	return args;
 }
 
+/*
+ * Finds the given redirection symbol.
+ * Argument(s):
+ *   int argc: The number of command line arguments.
+ *   char* argv[]: The command line arguments.
+ *   const char* symbol: The symbol to search for.
+ *   redir_sym* sym: A pointer to the struct to store the symbol and its index in.
+ */
 static void find_symbol(int argc, char* argv[], const char* symbol, redir_sym* rsym) {
 	for (unsigned int i = 1; i < (argc-1); i++) {
 		if (!strcmp(argv[i], symbol)) {
@@ -37,6 +54,14 @@ static void find_symbol(int argc, char* argv[], const char* symbol, redir_sym* r
 	}
 }
 
+/*
+ * Redirects the Standard Input of a program to be a given file.
+ * Argument(s):
+ *   int argc: The number of command line arguments.
+ *   char* argv[]: The command line arguments.
+ * Returns:
+ *   A 1 for Success or 0 as Failure.
+ */
 int redirect_in(int argc, char* argv[]) {
 	redir_sym isym = {NULL, 0};
 	find_symbol(argc, argv, "<", &isym);
@@ -60,6 +85,14 @@ int redirect_in(int argc, char* argv[]) {
 	} else return REDIRECT_FAILURE;
 }
 
+/*
+ * Redirects the Standard Output of a program to a given file.
+ * Argument(s):
+ *   int argc: The number of command line arguments.
+ *   char* argv[]: The command line arguments.
+ * Returns:
+ *   A 1 for Success or 0 as Failure.
+ */
 int redirect_out(int argc, char* argv[]) {
 	redir_sym osym = {NULL, 0};
 	find_symbol(argc, argv, ">", &osym);
@@ -94,6 +127,15 @@ int redirect_out(int argc, char* argv[]) {
 	} else return REDIRECT_FAILURE;
 }
 
+/*
+ * Redirects the Standard Output of a program to 
+ * be the Standard Input of another program.
+ * Argument(s):
+ *   int argc: The number of command line arguments.
+ *   char* argv[]: The command line arguments.
+ * Returns:
+ *   A 1 for Success or 0 as Failure.
+ */
 int redirect_pipe(int argc, char* argv[]) {
 	redir_sym psym = {NULL, 0};
 	find_symbol(argc, argv, "|", &psym);
