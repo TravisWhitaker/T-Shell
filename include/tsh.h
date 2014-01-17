66b67797b9c9fbabc8b5e68c64dccffa427376fd
Tyler Cromwell <tyler@csh.rit.edu>
2014-01-16 22:57:26 -0500

    Error messages are now colored red

--- include/tsh.h
+++ include/tsh.h
@@ -1,6 +1,8 @@
 #ifndef TSH_H
 #define TSH_H
 
+#define COLOR_RED	"\x1b[31m"
+#define COLOR_RESET	"\x1b[0m"
 #define BLANK_SPACE 32 // The ASCII value for the Space character.
 #define BUFFER_SIZE BUFSIZ/32 // New buffer size.
 #define STRING_END '\0' // Null terminator, marks end of a string.
@@ -16,3 +18,4 @@ static inline void release(void* ptr) {
 }
 
 #endif
+
