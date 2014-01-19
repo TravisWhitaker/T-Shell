2bc539fde497da36da54d751f76af74b6d971f4e
Tyler Cromwell <tyler@csh.rit.edu>
2014-01-19 16:58:45 -0500

    Added an Unknown type label

--- include/symtab.h
+++ include/symtab.h
@@ -6,11 +6,12 @@
 #include "data-structs/gentype.h"
 
 typedef enum type {
-	NUMBER = 1,      // Number data type (1, 3.14, -2, etc).
-	STRING = 2,      // String data type ("Hello", 'C', etc).
-	FUNC_VOID = 3,   // Type for a Function returning Void (foo(), bar(args), etc).
-	FUNC_NUMBER = 4, // Type for a Function returning a Number.
-	FUNC_STRING = 5, // Type for a Function returning a String.
+	UNKNOWN,	 // 0) Type label for an Unknown Data Type
+	NUMBER,      // 1) Number data type (1, 3.14, -2, etc).
+	STRING,      // 2) String data type ("Hello", 'C', etc).
+	FUNC_VOID,   // 3) Type for a Function returning Void (foo(), bar(args), etc).
+	FUNC_NUMBER, // 4) Type for a Function returning a Number.
+	FUNC_STRING  // 5) Type for a Function returning a String.
 } Type;
 
 typedef struct sym {
